603f391087137fcd79ed6346b4de8472ae6039e3
Krassen22 <krassen22@gmail.com>
2014-05-04 23:17:28 +0300

    Commit player

--- player/player.c
+++ player/player.c
@@ -6,7 +6,7 @@ void init_player(struct player_t *player)
 {
         player->hp_player = 30;
         init_mana(&player->mana_player);
-        
+
         for(player->last_t_card_in_hand = 0; player->last_t_card_in_hand <= 4; player->last_t_card_in_hand++)
 		{
 			player->cards_in_hand[player->last_t_card_in_hand] = draw_card(&player->deck_player);
@@ -49,7 +49,7 @@ void turn_begin(struct player_t *player)
 {
 	if (player->last_t_card_in_hand <= HANDSZ)
 	{
-	
+
 			if (player->deck_player.top != 0)
 			{
 					player->cards_in_hand[player->last_t_card_in_hand] = draw_card(&player->deck_player);
@@ -61,7 +61,7 @@ void turn_begin(struct player_t *player)
 			}
 
 	}
-	else 
+	else
 	{
 			printf("Hand is full!");
 	}
@@ -80,7 +80,7 @@ void play_card_from_hand(struct player_t *player, char *card_name)
 			player->cards_in_hand[e].power = 0;
 			player->cards_in_hand[e].mana_cost = 0;
 		}
-		
+
 	}
 }
 
@@ -96,42 +96,7 @@ void print_cards_in_hand(struct player_t player)
     }
 }
 
-int main()
-{
-struct card_t card_to_push;
-struct deck_t player_deck;
-struct player_t player;
-struct card_t one = { "card_one", 1, 7, 3 };
-struct card_t two = { "card_two", 3, 7, 10 };
-struct card_t three = { "card_three", 3, 2, 5 };
-struct card_t four = { "card_four", 1, 2, 2 };
-struct card_t five = { "card_five", 3, 2, 4 };
-struct card_t six = { "card_six", 5, 4, 1 };
-struct card_t seven = { "card_seven", 6, 2, 4 };
-init_deck(&player_deck);
-push_card(one, &player_deck);
-push_card(two, &player_deck);
-push_card(three, &player_deck);
-push_card(four, &player_deck);
-push_card(five, &player_deck);
-push_card(six, &player_deck);
-push_card(seven, &player_deck);
 
-print_stack(player_deck);
-init_player(&player);
-
-down_hp(&player,4);
-up_hp(&player,2);
-
-turn_begin(&player);
-turn_begin(&player);
-
-print_cards_in_hand(player);
-play_card_from_hand(&player, "card_four");
-print_cards_in_hand(player);
-
-    return 0;
-}
 
 
 
