0fb48f875aa1ffcd41a1b362f68e3e1bbe0871cb
Krassen22 <krassen22@gmail.com>
2014-05-24 22:39:53 +0300

    Commit board

--- board/board.c
+++ board/board.c
@@ -44,26 +44,24 @@ int can_play_card(struct board_t *board, struct player_t *pl, char *card_name, i
 
 int play_card(struct board_t *board, struct player_t *pl, int player, char *card_name, int num_lane)
 {
-	for (num_lane=0; num_lane < LANES;num_lane++)
+	int i;
+	for(i=0;i <= pl->last_t_card_in_hand ;i++)
 	{
-		int i;
-		for(i=0;i <= pl->last_t_card_in_hand ;i++)
+		if (pl->cards_in_hand[i].name == card_name && &board->board_game[player][num_lane] == 0 && pl->mana_player.current_mana >= pl->cards_in_hand[i].mana_cost )
 		{
-			if (pl->cards_in_hand[i].name == card_name && &board->board_game[player][num_lane] == 0 && pl->mana_player.current_mana >= pl->cards_in_hand[i].mana_cost )
-			{
-				board->board_game[player][num_lane].name = pl->cards_in_hand[i].name;	
-				board->board_game[player][num_lane].live = pl->cards_in_hand[i].live;	
-				board->board_game[player][num_lane].power = pl->cards_in_hand[i].power;	
-				board->board_game[player][num_lane].mana_cost = pl->cards_in_hand[i].mana_cost;	
-				
-				return 1; // we can play card 	
-			}
-			else
-			{
-				return 0; // we can NOT play card	
-			}
+			board->board_game[player][num_lane].name = pl->cards_in_hand[i].name;	
+			board->board_game[player][num_lane].live = pl->cards_in_hand[i].live;	
+			board->board_game[player][num_lane].power = pl->cards_in_hand[i].power;	
+			board->board_game[player][num_lane].mana_cost = pl->cards_in_hand[i].mana_cost;	
+			
+			return 1; // we can play card 	
+		}
+		else
+		{
+			return 0; // we can NOT play card	
 		}
 	}
+	
 }
 
 
