6c6423b45497f75cbb392fbf8ee5e960e7fb66c6
Krassen22 <krassen22@gmail.com>
2014-05-27 00:16:49 +0300

    Commit AI_IO

---
+++ AI_IO/AI_IO.h
@@ -0,0 +1,15 @@
+#ifndef __AI_IO__
+#define __AI_IO__
+#include "..\card\card.h"
+#include "..\deck\deck.h"
+
+
+
+
+void make_deck();
+void read_card_s_names_deck();
+//void load_deck();
+
+
+
+#endif
